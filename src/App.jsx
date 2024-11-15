dc24bf64d3d237be0f7ca9403a1846f044dfecf3
Tremaine McKinley <49395726+tremckinley@users.noreply.github.com>
2024-11-15 03:43:01 +0000

    corrected tailwind installation

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+      <div className='text-green-300'>
+        Hello World
+      </div>
+    </>
+  )
+}
+
+export default App
