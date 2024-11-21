ae6b4dc45417602e51105092603fbbac47f12a0d
Tremaine McKinley <49395726+tremckinley@users.noreply.github.com>
2024-11-21 03:32:40 +0000

    state card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import { useState } from 'react'
+import ProjectCard from './assets/ProjectCard'
 
 function App() {
 
   return (
     <>
-      <div className='text-green-300'>
-        Hello World
+      <div className="w-full border flex flex-col items-center">
+      <nav className="bg-emerald-500 w-full max-w-7xl h-16">
+      dfmf;m
+      </nav>
+      <div className='m-20 flex flex-wrap justify-center'>
+        <ProjectCard></ProjectCard>
+        <ProjectCard></ProjectCard>
+        <ProjectCard></ProjectCard>
+        <ProjectCard></ProjectCard>
+        <ProjectCard></ProjectCard>
+      </div>
       </div>
     </>
   )
---
+++ src/assets/ProjectCard.jsx
@@ -0,0 +1,18 @@
+export default function ProjectCard(props) {
+    const {project} = props;
+    return (
+      <div className='flex flex-col bg-white rounded-lg p-1 border border-black w-fit'>
+        <div className='flex mb-4'>
+            <div className="bg-orange-100 w-48"></div>
+          {/* <img src={project.image} className='flex border border-gray-300 rounded-lg w-64' alt={`information about ${project.name}`} /> */}
+          <div className='flex flex-col w-64 px-4'>
+            <div className='text-4xl mb-4'>Fright Night</div>
+            <div className='flex justify-between'><div className='font-bold'>Nickname</div><div>Guessing Game</div></div>
+            <div className='flex justify-between'><div className='font-bold'>Capital</div><div>Here you go</div></div>
+             <div className='flex justify-between'><div className='font-bold'>Population</div><div>So many</div></div>
+          </div>
+        </div>
+        <div>Description</div>
+      </div>
+    )
+  }
