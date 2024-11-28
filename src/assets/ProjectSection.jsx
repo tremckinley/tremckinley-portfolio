97d7febe57787f039169ac2da1efbda96bc8c9b8
Tremaine McKinley <49395726+tremckinley@users.noreply.github.com>
2024-11-28 19:48:00 +0000

    light refactor

--- src/assets/ProjectSection.jsx
+++ src/assets/ProjectSection.jsx
@@ -12,7 +12,6 @@ export default function ProjectSection() {
                     <ProjectCard key={project.title} project={project} />
                 )
                 })
-
             }
 
         </div>
