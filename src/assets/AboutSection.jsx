65b2b311a0c20bdc97d9e4dbfac88d5bb898d4e1
tremckinley <tremckinley901@gmail.com>
2025-04-24 21:00:36 -0500

    Edit About me tagline

--- src/assets/AboutSection.jsx
+++ src/assets/AboutSection.jsx
@@ -6,7 +6,7 @@ export default function AboutSection() {
         <br/><br/>Currently, I’m helping shape the future of Memphis government services as a Business Services Analyst for the Shelby County Division of Planning and Development. My work focuses on configuring and enhancing SaaS platforms to streamline processes, improve accessibility, and drive meaningful change for internal and external customers. No day is the same and I approach every challenge with curiosity, determination, and a diverse toolkit. Previously, I was a Director of Finance & Technology for a local charter school. Beyond my professional roles, I am a lifelong learner with experience living and working abroad. 
         <br/><br/>I am consistently pursuing personal projects that combine creativity and technical expertise to advance my personal and professional dreams. You can usually find me completing a course, networking with smarter people, or developing something just because I think it will be cool. Whether I’m troubleshooting code, improving user experiences, or building tools to make civic tech more inclusive, I’m committed to leaving things better than I found them. 
         <br/><br/>
-        <center><strong>Let’s connect! I’m sure there's something great we can do together.</strong></center>
+        <center><strong>Let’s connect! There's something great we can do together.</strong></center>
         </p>
       
         </section>
