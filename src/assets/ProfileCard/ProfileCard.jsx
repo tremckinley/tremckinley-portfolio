8761b3ca7f6e84e81f6ac67477f46acd8d9c1937
tremckinley <tremckinley901@gmail.com>
2025-12-26 15:42:24 -0600

    Adjust header alignment

--- src/assets/ProfileCard/ProfileCard.jsx
+++ src/assets/ProfileCard/ProfileCard.jsx
@@ -3,11 +3,12 @@ import ViewSelector from "./ViewSelector"
 export default function ProfileCard(props) {
     const { state } = props
     return (
-        <div className="xl:pl-12 flex flex-col lg:h-72 items-start justify-between p-4 pb-0 text-gray-600 shadow-xl bg-[#dfdfdf]" role="banner border border-gray-500">
-            <div className="flex flex-row justify-center md:justify-start items-center w-full">
+        <div className="w-full mx-auto flex flex-col items-center justify-between p-4 pb-0 text-gray-600 shadow-xl" role="banner border border-gray-500">
+            <div className="w-4xl w-full max-w-6xl">
+            <div className="w-full flex flex-row justify-center md:justify-start items-center">
                 <div className="flex flex-col lg:flex-row-reverse items-center">
-                    <img className="rounded-[40%] max-h-28 lg:max-h-48" alt="profile image of me" src="/headshot.jpeg" style={{ boxShadow: "0px 0px 10px var(--accent-secondary)" }} />
-                    <div className="flex lg:flex-col lg:items-end lg:pr-2 justify-between w-24 lg:text-3xl text-xl mx-auto lg:mx-0">
+                    <img className="rounded-[40%] max-h-28 lg:max-h-24" alt="profile image of me" src="/headshot.jpeg" style={{ boxShadow: "0px 0px 10px var(--accent-secondary)" }} />
+                    <div className="flex lg:flex-col lg:items-end lg:pr-2 justify-between w-fit lg:text-xl text-xl mx-auto lg:mx-0">
                         <a href="https://www.linkedin.com/in/tremaine-mckinley/" target="_blank"><i className="contact-icon fa-brands fa-linkedin" title="Visit LinkedIn page"></i></a>
                         <button title="Email Me - Copy to clipboard" onClick={() => {
                             navigator.clipboard.writeText("tremckinley901@gmail.com")
@@ -25,14 +26,13 @@ export default function ProfileCard(props) {
                     </div>
                 </div>
                 <div className="ml-4 text-left">
-                    <h1 className="heading-text mt-2 mb-2 text-3xl lg:text-6xl">Tremaine McKinley</h1>
-                    <h2 className="text-sm lg:text-2xl">Civic Technologist. Web Dev. Impact Player.</h2>
-                    <a href="mailto:tremckinley901@gmail.com" className="block mt-2 text-sm lg:text-2xl font-semibold text-[dodgerblue] hover:underline cursor-pointer">Let's get to it.</a>
+                    <h1 className="heading-text mt-2 mb-2 text-3xl lg:text-3xl">Tremaine McKinley</h1>
+                    <h2 className="text-sm lg:text-lg">Civic Technologist. Web Dev. Impact Player.</h2>
                 </div>
             </div>
 
             <ViewSelector state={state} />
-
+            </div>
         </div>
     )
 }
