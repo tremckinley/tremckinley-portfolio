49c01d97f6cb749dd4d49cf117e4991be2b8754b
tremckinley <tremckinley901@gmail.com>
2025-12-27 23:29:06 -0600

    Center articles h3 element

--- src/assets/BlogSection.jsx
+++ src/assets/BlogSection.jsx
@@ -5,7 +5,7 @@ import { blogs } from "../utils/blogs";
 export default function BlogSection() {
     return (
         <>
-        <h3 className="text-2xl font-bold text-accent-secondary mb-6">Featured Articles</h3>
+        <h3 className="text-2xl font-bold text-accent-secondary mb-6 text-center">Featured Articles</h3>
         <section>
         <div className="flex flex-wrap justify-center">
             {!blogs ? <p>Error: Projects not loaded</p> : 
